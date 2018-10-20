d783c40893241ebec78168a64bdf0bdeb8ef5f7f
Gus Vroustouris <gvrousto@purdue.edu>
2018-10-20 15:34:55 -0400

    connect to API

--- backend/analyze.py
+++ backend/analyze.py
@@ -1,13 +1,9 @@
-from clarifai import rest
 from clarifai.rest import ClarifaiApp
-import server_config
 
-# Create your API key in your account's `Manage your API keys` page:
-# https://clarifai.com/developer/account/keys
+app = ClarifaiApp(api_key='a2596d582f04477d847353f2c60e4ed1')
 
-app = ClarifaiApp(api_key=server_config.CLARIFAI_TOKEN)
 # get the general model
 model = app.models.get("general-v1.3")
 
 # predict with the model
-model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg')
+print(model.predict_by_url(url='https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/27654941_592960624369581_6880587150061114791_n.jpg?_nc_cat=103&_nc_ht=scontent-ort2-1.xx&oh=f924f97b0fdd21d054056763988861c1&oe=5C5CEBE7').outputs.data.concepts)
